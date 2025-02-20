db29e9e937caa48f47285cba3a2b4dc66289ff81
HEUREUX Axel <axel.heureux@laplateforme.io>
2025-02-20 10:03:28 +0100

    📝 Mise a jour de la BDD

--- pages/database/livreor.sql
+++ pages/database/livreor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 fév. 2025 à 13:37
+-- Généré le : jeu. 20 fév. 2025 à 09:02
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -35,7 +35,15 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `comment`, `id_user`, `date`) VALUES
+(1, 'Un avis mitigé ', 4, '2025-02-19 13:07:14'),
+(2, 'C\'est vraiment très inquiétant ! ', 5, '2025-02-19 13:49:59');
 
 -- --------------------------------------------------------
 
@@ -49,7 +57,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`) VALUES
+(5, 'Gianni', '$2y$10$Ch/9AS07XQmmMUpvZuLR8OOXFA4PfUl8/.ifATrr74yz53tU4JC3y'),
+(4, 'Axel', '$2y$10$Ch/9AS07XQmmMUpvZuLR8OOXFA4PfUl8/.ifATrr74yz53tU4JC3y');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
